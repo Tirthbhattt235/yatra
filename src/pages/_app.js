540d6b9ecdd18d03da8eb5cf714f75bff5f5ae94
tirth bhatt <btirth240@gmail.com>
2024-09-17 19:43:12 +0530

    Initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
-
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
